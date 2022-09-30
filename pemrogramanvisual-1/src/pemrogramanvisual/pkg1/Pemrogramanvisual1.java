99d044a7d01fb6d0bb44af452a252dc390ed1f09
randyazharalman <101396152+randyazharalman@users.noreply.github.com>
2022-09-30 10:15:17 +0700

    Add files via upload

---
+++ pemrogramanvisual-1/src/pemrogramanvisual/pkg1/Pemrogramanvisual1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pemrogramanvisual.pkg1;
+
+import NewJFrame;
+
+/**
+ *
+ * @author Labkom-M15
+ */
+public class Pemrogramanvisual1 {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic 
+       new NewJFrame().setVisible(true);
+        System.out.println("hello world"); 
+    }
+    
+}
